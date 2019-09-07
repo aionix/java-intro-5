ff75e5d52a371554d919d635ff8645e94636fa96
artem.buchynskyi <art231188@gmail.com>
2019-09-07 14:58:34 +0300

    hometask: "Работа с коллекциями"

--- src/main/java/lesson04/CollectionsHomeWork.java
+++ src/main/java/lesson04/CollectionsHomeWork.java
@@ -1,4 +1,34 @@
-package Collections;
+package lesson04;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 
 public class CollectionsHomeWork {
+
+    public static void main(String[] args) {
+        ArrayList<String> cars = new ArrayList<>();
+        cars.add("Audi");
+        cars.add("BMW");
+        cars.add("Mercedes");
+        cars.add("Lexus");
+
+
+        cars.remove("BMW");
+        System.out.println(cars.contains("BMW"));
+
+        Iterator iterator = cars.iterator();
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next()+ " ");
+        }
+
+        Collections.sort(cars);
+        System.out.print(" =====>sorted====> ");
+
+        Iterator iterator2 = cars.iterator();
+        while (iterator2.hasNext()) {
+            System.out.print(iterator2.next()+ " ");
+        }
+
+    }
 }
